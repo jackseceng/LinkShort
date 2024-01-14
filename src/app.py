dec391d2b49b1d7dc2f5969b17d26f5c21d79682
JackEdwards-ITV <je@jacksec.engineer>
2024-01-14 22:11:15 +0000

    working redis setup, removed postgres

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import logging
 
 import url_mgmt as urls
 
-import db_mgmt as db
+import redis_mgmt as db
 
 app = Flask(__name__)
 
@@ -16,11 +16,9 @@ def input_url():
     """Main page process"""
     match request.method:
         case 'GET':
-            logging.warning("Home GET request")
             resp = make_response(render_template('index.html')) # Return index page for GET method
             return resp
         case 'POST':
-            logging.warning("Home POST request")
             user_input = dict(request.form.to_dict())
             ui_log = (f"User input: {user_input}")
             logging.warning(ui_log)
@@ -39,15 +37,9 @@ def input_url():
                 logging.warning(error)
                 resp = make_response(render_template('index.html', error_reason=error))
                 return resp
-            logging.warning("Generating path initial value")
             path = urls.generate_path(str(user_input))
-            logging.warning("Path generated")
-            logging.warning(path)
             # Generate path for shortened URL
-            logging.warning("Initial check for collisions")
-            check = db.check_link(path)
-            logging.warning(check)
-            while db.check_link(path) is True:
+            while db.get_link(path) is True:
                 logging.warning("Collision detected")
                 # Check for collisions with existing paths
                 path = urls.generate_path(str(user_input))
@@ -66,7 +58,6 @@ def input_url():
             return resp
             # Render link page with URL
         case _:
-            logging.warning("Catch all home 500 error")
             resp = make_response(render_template('500.html', code=500))
             return resp
             # Catch all to handle unexpected errors, render 500 error page to front end
@@ -75,11 +66,8 @@ def input_url():
 @app.route('/<arg>', methods=['GET'])  # Handle any GET requests for paths
 def redirect_url(arg):
     """Redirect logic for any GET requests to any URI on top of base URL"""
-    logging.warning("Redirect GET request")
     path = str(arg)
-    logging.warning(path)
     if urls.check_url(path, 3) is False: # Run check to see if path format is valid
-        logging.warning("Invalid path")
         resp = make_response(render_template('404.html', code=404))
         return resp
         # Return 404 not found if URL is invalid
@@ -87,11 +75,9 @@ def redirect_url(arg):
     link = db.get_link(arg)
     if link is not False: # Fetch the original URL
         output = (f"Redirecting to original URL {link}")
-        logging.warning(output)
         resp = make_response(redirect(link, code=302))
         return resp
         # Return 302 response to client, with original URL as redirect
-    logging.warning("Path not found")
     resp = make_response(render_template('404.html', code=404))
     return resp
     # Catch all 404 error handler for unknown paths
--- src/redis_mgmt.py
+++ src/redis_mgmt.py
@@ -1,71 +1,56 @@
 import time
-from rediscluster import RedisCluster
-
-# Test this using a python container in the same docker network as the redis cluster defined in the docker-compose file (ls-net)
-
-def get_redis_connection():
-    # Update with your Redis Sentinel service or container names
-    startup_nodes = [
-        {"host": "sentinel-0"},
-        {"host": "sentinel-1"},
-        {"host": "sentinel-2"}
-    ]
-
-    # Create a RedisCluster instance
-    redis_cluster = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
-
-    return redis_cluster
+import redis
 
 
-def insert_link(path, link, max_retries=3, retry_delay=1):
+# Create redis connection object
+r = redis.Redis(host="redis-master", port=6379, username="default", password="master-password", db=0)
+
+
+def insert_link(path, link, max_retries=3):
     for attempt in range(max_retries):
         try:
-            # Connect to the Redis cluster
-            redis_connection = get_redis_connection()
-
             # Check if the key already exists
-            if redis_connection.exists(path):
+            if r.exists(path):
                 # Cancel operation if it already exists
                 print(f"Key '{path}' already exists. Skipping set operation.")
+                break
             else:
                 # Set a key-value pair
-                redis_connection.set(path, link)
+                r.set(path, link)
                 print(f"Value inserted for key: '{path}': {link}")
+                break
 
         except Exception as e:
             print(f"Error: {e}")
             # Retry logic
             if attempt < max_retries - 1:
-                print(f"Retrying in {retry_delay} seconds...")
-                time.sleep(retry_delay)
+                print("Retrying in 1 seconds...")
+                time.sleep(1)
             else:
                 print("Max retries reached. Operation failed.")
 
         finally:
             # Close the Redis connection
-            redis_connection.close()
+            r.close()
 
 
-def get_link(path, max_retries=3, retry_delay=1):
-     for attempt in range(max_retries):
+def get_link(path, max_retries=3):
+    for attempt in range(max_retries):
         try:
-            # Connect to the Redis cluster
-            redis_connection = get_redis_connection()
-
             # Get the value for the key
-            retrieved_value = redis_connection.get(path)
-
+            retrieved_value = r.get(path)
             print(f"Retrieved value for key '{path}': {retrieved_value}")
+            return retrieved_value
 
         except Exception as e:
             print(f"Error: {e}")
             # Retry logic
             if attempt < max_retries - 1:
-                print(f"Retrying in {retry_delay} seconds...")
-                time.sleep(retry_delay)
+                print("Retrying in 1 seconds...")
+                time.sleep(1)
             else:
                 print("Max retries reached. Operation failed.")
 
         finally:
             # Close the Redis connection
-            redis_connection.close()
+            r.close()
