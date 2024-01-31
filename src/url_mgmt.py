f8c5174fe5d535869f39f9923b2a5664669b622d
Jack Edwards <je@jacksec.engineer>
2024-01-31 14:32:52 +0000

    linter fix

--- src/url_mgmt.py
+++ src/url_mgmt.py
@@ -1,13 +1,14 @@
 """Hashing and input checking module"""
 import logging
 from re import sub
-import bcrypt
+
+from bcrypt import gensalt, hashpw
 
 
 def generate_path(original_url):
     """Generate path path value"""
-    hash_object = bcrypt.hashpw(original_url.encode(), bcrypt.gensalt(rounds=15))
-    hash_string = sub(r'\W+', '', str(hash_object, encoding='utf-8'))
+    hash_object = hashpw(original_url.encode(), gensalt(rounds=15))
+    hash_string = sub(r"\W+", "", str(hash_object, encoding="utf-8"))
     # Return last 7 characters of resulting generated path
     return hash_string[-7:]
 
