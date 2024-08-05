a80058ec115349f587bd0191a6909e0c5472ced7
JackEdwards-ITV <je@jacksec.engineer>
2024-08-05 19:56:53 +0100

    adding new k8s configurations

--- src/code/app.py
+++ src/code/app.py
@@ -80,4 +80,4 @@ def add_security_headers(resp):
 
 # # Flask app DEV main function
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=80)
