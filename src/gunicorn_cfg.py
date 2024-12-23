3ef023406f3b4200baa1bcc8669d46d9995c5957
JackEdwards-ITV <je@jacksec.engineer>
2024-12-23 20:27:14 +0000

    add docstring

--- src/gunicorn_cfg.py
+++ src/gunicorn_cfg.py
@@ -1,17 +1,16 @@
+"""Configuration module for Gunicorn"""
 import os
 
 
+workers = int(os.environ.get("GUNICORN_PROCESSES", "2"))
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+threads = int(os.environ.get("GUNICORN_THREADS", "4"))
 
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:8080")
 
 
+forwarded_allow_ips = "*"
 
-forwarded_allow_ips = '*'
-
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+secure_scheme_headers = {"X-Forwarded-Proto": "https"}
