45957643729cbaeeb4adb0e822cf4032074e443c
Jack Edwards <je@jacksec.engineer>
2025-07-18 22:27:21 +0100

    style(prettier-js): Apply prettier fixes to javascript files, rename qrcode js file

--- app/static/main.js
+++ app/static/main.js
@@ -1,20 +1,20 @@
-const errorValue = document.getElementById('errorreason').getAttribute('value')
+const errorValue = document.getElementById("errorreason").getAttribute("value");
 
-if (errorValue === 'whitespace') {
-  showNotification("URLs can't contain whitespace")
-} else if (errorValue === 'insecure') {
-  showNotification('Links must begin with HTTPS')
-} else if (errorValue === 'badsite') {
-  showNotification('URL has a bad reputaion')
+if (errorValue === "whitespace") {
+  showNotification("URLs can't contain whitespace");
+} else if (errorValue === "insecure") {
+  showNotification("Links must begin with HTTPS");
+} else if (errorValue === "badsite") {
+  showNotification("URL has a bad reputaion");
 }
 
-function showNotification (message) {
-  const notification = document.getElementById('notification')
-  notification.textContent = message
-  notification.classList.add('show')
+function showNotification(message) {
+  const notification = document.getElementById("notification");
+  notification.textContent = message;
+  notification.classList.add("show");
 
   // Remove the notification after 5 seconds
   setTimeout(() => {
-    notification.classList.remove('show')
-  }, 5000)
+    notification.classList.remove("show");
+  }, 5000);
 }
