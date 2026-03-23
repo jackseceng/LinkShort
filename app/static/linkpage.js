0ecd6890995d07ba02007709cc0d76ba8fa75cb5
Jack Edwards <je@jacksec.engineer>
2026-03-23 22:20:24 +0000

    fix(html-format): updated html formatting for link and stats pages, separated out copy js to separate file

--- app/static/linkpage.js
+++ app/static/linkpage.js
@@ -1,25 +1,3 @@
-/* global showNotification, QRCode */
-/* eslint no-undef: "error" */
-
-const copyValue = document.getElementById("copybtn");
-
-copyValue.addEventListener("click", () => {
-  const copytextbox = document.getElementById("link");
-  const copiedvalue = copytextbox.value;
-  console.log("Copying link to clipboard");
-  navigator.clipboard
-    .writeText(copiedvalue)
-    .then(() => {
-      // Replace alert with custom notification
-      // window.location.href = "/";
-      showNotification("Copied to clipboard");
-    })
-    .catch((err) => {
-      showNotification("Failed to copy to clipboard");
-      console.error("Failed to copy: ", err);
-    });
-});
-
 const linkqrcode = new QRCode("qrcode", {
   width: 160,
   height: 160,
